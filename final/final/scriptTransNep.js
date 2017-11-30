cda6a458806ced4f2299dd892ed951a770206cb5
anita <anitalama07@gmail.com>
2017-11-30 09:08:36 +0545

    final project

---
+++ final/final/scriptTransNep.js
@@ -0,0 +1,1286 @@
+let obj = {
+
+//VOWELS
+	अ: {
+		value: 'a'
+		},
+	आ: {
+		value: 'aa'
+		},
+	इ: {
+		value: 'e'
+		},
+	उ: {
+		value: 'u'
+		},
+	ए: {
+		value: 'ae'
+		},
+	ऐ: {
+		value: 'ai'
+		},
+	ओ: {
+		value: 'o'
+		},
+	औ: {
+		value: 'au'
+		},
+
+
+//CONSONANTS
+	क: {
+		value: 'ka'
+		},
+	का: {
+		value: 'kaa'
+		},
+	'िक': {
+		value: 'ki'
+		},
+	की: {
+		value: 'kii'
+		},
+	कु: {
+		value: 'ku'
+		},
+	कू: {
+		value: 'koo'
+		},
+	के: {
+		value: 'ke'
+		},
+	कै: {
+		value: 'kai'
+		},
+	को: {
+		value: 'ko'
+	},
+	कौ: {
+		value: 'kau'
+	},
+		
+	ख: {
+		value: 'kha'
+		},
+	खा: {
+		value: 'khaa'
+		},
+	'िख': {
+		value: 'khi'
+		},
+	खी: {
+		value: 'khii'
+		},
+	खु: {
+		value: 'khu'
+		},
+	खू: {
+		value: 'khoo'
+		},
+	खे: {
+		value: 'khe'
+		},
+	खै: {
+		value: 'khai'
+		},
+	खो: {
+		value: 'kho'
+	},
+	खौ: {
+		value: 'khau'
+	},
+	ग: {
+		value: 'ga'
+		},
+	गा: {
+		value: 'gaa'
+		},
+	'िग': {
+		value: 'gi'
+		},
+	गी: {
+		value: 'gii'
+		},
+	गु: {
+		value: 'gu'
+		},
+	गू: {
+		value: 'goo'
+		},
+	गे: {
+		value: 'ge'
+		},
+	गै: {
+		value: 'gai'
+		},
+	गो: {
+		value: 'go'
+	},
+	गौ: {
+		value: 'gau'
+	},
+	घ: {
+		value: 'gha'
+		},
+	घा: {
+		value: 'ghaa'
+		},
+	'िघ': {
+		value: 'ghi'
+		},
+	घी: {
+		value: 'ghii'
+		},
+	घु: {
+		value: 'ghu'
+		},
+	घू: {
+		value: 'ghoo'
+		},
+	घे: {
+		value: 'ghe'
+		},
+	घै: {
+		value: 'ghai'
+		},
+	घो: {
+		value: 'gho'
+	},
+	घौ: {
+		value: 'ghau'
+	},
+	ङ: {
+		value: 'ng'
+		},
+	च: {
+		value: 'ca'
+		},
+	चा: {
+		value: 'caa'
+		},
+	'िच': {
+		value: 'ci'
+		},
+	ची: {
+		value: 'cii'
+		},
+	चु: {
+		value: 'cu'
+		},
+	चू: {
+		value: 'coo'
+		},
+	चे: {
+		value: 'ce'
+		},
+	चै: {
+		value: 'cai'
+		},
+	चो: {
+		value: 'co'
+	},
+	चौ: {
+		value: 'cau'
+	},
+	छ: {
+		value: 'cha'
+		},
+	छा: {
+		value: 'chaa'
+		},
+	'िछ': {
+		value: 'chi'
+		},
+	छी: {
+		value: 'chii'
+		},
+	छु: {
+		value: 'chu'
+		},
+	छू: {
+		value: 'choo'
+		},
+	छे: {
+		value: 'che'
+		},
+	छै: {
+		value: 'chai'
+		},
+	छो: {
+		value: 'cho'
+	},
+	छौ: {
+		value: 'chau'
+	},
+	ज: {
+		value: 'ja'
+		},
+	जा: {
+		value: 'jaa'
+		},
+	'िज': {
+		value: 'ji'
+		},
+	जी: {
+		value: 'jii'
+		},
+	जु: {
+		value: 'ju'
+		},
+	जू: {
+		value: 'joo'
+		},
+	जे: {
+		value: 'je'
+		},
+	जै: {
+		value: 'jai'
+		},
+	जो: {
+		value: 'jo'
+	},
+	जौ: {
+		value: 'jau'
+	},
+	झ: {
+		value: 'jha'
+		},
+	झा: {
+		value: 'jhaa'
+		},
+	'िझ': {
+		value: 'jhi'
+		},
+	झी: {
+		value: 'jhii'
+		},
+	झु: {
+		value: 'jhu'
+		},
+	झू: {
+		value: 'jhoo'
+		},
+	झे: {
+		value: 'jhe'
+		},
+	झै: {
+		value: 'jhai'
+		},
+	झो: {
+		value: 'jho'
+	},
+	झौ: {
+		value: 'jhau'
+	},
+	ञ: {
+		value: 'Na'
+		},
+	ट: {
+		value: 'Ta'
+		},
+	टा: {
+		value: 'Taa'
+		},
+	'िट': {
+		value: 'Ti'
+		},
+	टी: {
+		value: 'Tii'
+		},
+	टु: {
+		value: 'Tu'
+		},
+	टू: {
+		value: 'Too'
+		},
+	टे: {
+		value: 'Te'
+		},
+	टै: {
+		value: 'Tai'
+		},
+	टो: {
+		value: 'To'
+	},
+	टौ: {
+		value: 'Tau'
+	},
+	ठ: {
+		value: 'Tha'
+		},
+	ठा: {
+		value: 'Thaa'
+		},
+	'िठ': {
+		value: 'Thi'
+		},
+	ठी: {
+		value: 'Thii'
+		},
+	ठु: {
+		value: 'Thu'
+		},
+	ठू: {
+		value: 'Thoo'
+		},
+	ठे: {
+		value: 'The'
+		},
+	ठै: {
+		value: 'Thai'
+		},
+	ठो: {
+		value: 'Tho'
+	},
+	ठौ: {
+		value: 'Thau'
+	},
+	ड: {
+		value: 'Da'
+		},
+	डा: {
+		value: 'Daa'
+		},
+	'िड': {
+		value: 'Di'
+		},
+	डी: {
+		value: 'Dii'
+		},
+	डु: {
+		value: 'Du'
+		},
+	डू: {
+		value: 'Doo'
+		},
+	डे: {
+		value: 'De'
+		},
+	डै: {
+		value: 'Dai'
+		},
+	डो: {
+		value: 'Do'
+	},
+	डौ: {
+		value: 'Dau'
+	},
+	ढ: {
+		value: 'Dha'
+		},
+	ढा: {
+		value: 'Dhaa'
+		},
+	'िढ': {
+		value: 'Dhi'
+		},
+	ढी: {
+		value: 'Dhii'
+		},
+	ढु: {
+		value: 'Dhu'
+		},
+	ढू: {
+		value: 'Dhoo'
+		},
+	ढे: {
+		value: 'Dhe'
+		},
+	ढै: {
+		value: 'Dhai'
+		},
+	ढो: {
+		value: 'Dho'
+	},
+	ढौ: {
+		value: 'Dhau'
+	},
+	ण: {
+		value: 'Na'
+		},
+	त: {
+		value: 'ta'
+		},
+	ता: {
+		value: 'taa'
+		},
+	'ित': {
+		value: 'ti'
+		},
+	ती: {
+		value: 'tii'
+		},
+	तु: {
+		value: 'tu'
+		},
+	तू: {
+		value: 'too'
+		},
+	ते: {
+		value: 'te'
+		},
+	तै: {
+		value: 'tai'
+		},
+	तो: {
+		value: 'to'
+	},
+	तौ: {
+		value: 'tau'
+	},
+	थ: {
+		value: 'tha'
+		},
+	था: {
+		value: 'thaa'
+		},
+	'िथ': {
+		value: 'thi'
+		},
+	थी: {
+		value: 'thii'
+		},
+	थु: {
+		value: 'thu'
+		},
+	थू: {
+		value: 'thoo'
+		},
+	थे: {
+		value: 'the'
+		},
+	थै: {
+		value: 'thai'
+		},
+	थो: {
+		value: 'tho'
+	},
+	थौ: {
+		value: 'thau'
+	},
+	द: {
+		value: 'da'
+		},
+	दा: {
+		value: 'daa'
+		},
+	'िद': {
+		value: 'di'
+		},
+	दी: {
+		value: 'dii'
+		},
+	दु: {
+		value: 'du'
+		},
+	दू: {
+		value: 'doo'
+		},
+	दे: {
+		value: 'de'
+		},
+	दै: {
+		value: 'dai'
+		},
+	दो: {
+		value: 'do'
+	},
+	दौ: {
+		value: 'dau'
+	},
+	ध: {
+		value: 'dha'
+		},
+	धा: {
+		value: 'dhaa'
+		},
+	'िध': {
+		value: 'dhi'
+		},
+	धी: {
+		value: 'dhii'
+		},
+	धु: {
+		value: 'dhu'
+		},
+	धू: {
+		value: 'dhoo'
+		},
+	धे: {
+		value: 'dhe'
+		},
+	धै: {
+		value: 'dhai'
+		},
+	धो: {
+		value: 'dho'
+	},
+	धौ: {
+		value: 'dhau'
+	},
+	न: {
+		value: 'na'
+		},
+	ना: {
+		value: 'naa'
+		},
+	'िन': {
+		value: 'ni'
+		},
+	नी: {
+		value: 'nii'
+		},
+	नु: {
+		value: 'nu'
+		},
+	नू: {
+		value: 'noo'
+		},
+	ने: {
+		value: 'ne'
+		},
+	नै: {
+		value: 'nai'
+		},
+	नो: {
+		value: 'no'
+	},
+	नौ: {
+		value: 'nau'
+	},
+	प: {
+		value: 'pa'
+		},
+	पा: {
+		value: 'paa'
+		},
+	'िप': {
+		value: 'pi'
+		},
+	पी: {
+		value: 'pii'
+		},
+	पु: {
+		value: 'pu'
+		},
+	पू: {
+		value: 'poo'
+		},
+	पे: {
+		value: 'pe'
+		},
+	पै: {
+		value: 'pai'
+		},
+	पो: {
+		value: 'po'
+	},
+	पौ: {
+		value: 'pau'
+	},
+	फ: {
+		value: 'pha'
+		},
+	फा: {
+		value: 'phaa'
+		},
+	'िफ': {
+		value: 'phi'
+		},
+	फी: {
+		value: 'phii'
+		},
+	फु: {
+		value: 'phu'
+		},
+	फू: {
+		value: 'phoo'
+		},
+	फे: {
+		value: 'phe'
+		},
+	फै: {
+		value: 'phai'
+		},
+	फो: {
+		value: 'pho'
+	},
+	फौ: {
+		value: 'phau'
+	},
+	फ: {
+		value: 'fa'
+		},
+	फा: {
+		value: 'faa'
+		},
+	'िफ': {
+		value: 'fi'
+		},
+	फी: {
+		value: 'fii'
+		},
+	फु: {
+		value: 'fu'
+		},
+	फू: {
+		value: 'foo'
+		},
+	फे: {
+		value: 'fe'
+		},
+	फै: {
+		value: 'fai'
+		},
+	फो: {
+		value: 'fo'
+	},
+	फौ: {
+		value: 'fau'
+	},
+	
+	ब: {
+		value: 'ba'
+		},
+	बा: {
+		value: 'baa'
+		},
+	'िब': {
+		value: 'bi'
+		},
+	बी: {
+		value: 'bii'
+		},
+	बु: {
+		value: 'bu'
+		},
+	बू: {
+		value: 'boo'
+		},
+	बे: {
+		value: 'be'
+		},
+	बै: {
+		value: 'bai'
+		},
+	बो: {
+		value: 'bo'
+	},
+	बौ: {
+		value: 'bau'
+	},
+	भ: {
+		value: 'bha'
+		},
+	भा: {
+		value: 'bhaa'
+		},
+	'िभ': {
+		value: 'bhi'
+		},
+	भी: {
+		value: 'bhii'
+		},
+	भु: {
+		value: 'bhu'
+		},
+	भू: {
+		value: 'bhoo'
+		},
+	भे: {
+		value: 'bhe'
+		},
+	भै: {
+		value: 'bhai'
+		},
+	भो: {
+		value: 'bho'
+	},
+	भौ: {
+		value: 'bhau'
+	},
+	म: {
+		value: 'ma'
+		},
+	मा: {
+		value: 'maa'
+		},
+	'िम': {
+		value: 'mi'
+		},
+	मी: {
+		value: 'mii'
+		},
+	मु: {
+		value: 'mu'
+		},
+	मू: {
+		value: 'moo'
+		},
+	मे: {
+		value: 'me'
+		},
+	मै: {
+		value: 'mai'
+		},
+	मो: {
+		value: 'mo'
+	},
+	मौ: {
+		value: 'mau'
+	},
+	य: {
+		value: 'ya'
+		},
+	या: {
+		value: 'yaa'
+		},
+	'िय': {
+		value: 'yi'
+		},
+	यी: {
+		value: 'yii'
+		},
+	यु: {
+		value: 'yu'
+		},
+	यू: {
+		value: 'yoo'
+		},
+	ये: {
+		value: 'ye'
+		},
+	यै: {
+		value: 'yai'
+		},
+	यो: {
+		value: 'yo'
+	},
+	यौ: {
+		value: 'yau'
+	},
+	र: {
+		value: 'ra'
+		},
+	रा: {
+		value: 'raa'
+		},
+	'िर': {
+		value: 'ri'
+		},
+	री: {
+		value: 'rii'
+		},
+	रु: {
+		value: 'ru'
+		},
+	रू: {
+		value: 'roo'
+		},
+	रे: {
+		value: 're'
+		},
+	रै: {
+		value: 'rai'
+		},
+	रो: {
+		value: 'ro'
+	},
+	रौ: {
+		value: 'rau'
+	},
+	ल: {
+		value: 'la'
+		},
+	ला: {
+		value: 'laa'
+		},
+	'िल': {
+		value: 'li'
+		},
+	ली: {
+		value: 'lii'
+		},
+	लु: {
+		value: 'lu'
+		},
+	लू: {
+		value: 'loo'
+		},
+	ले: {
+		value: 'le'
+		},
+	लै: {
+		value: 'lai'
+		},
+	लो: {
+		value: 'lo'
+	},
+	लौ: {
+		value: 'lau'
+	},
+	व: {
+		value: 'va'
+		},
+	वा: {
+		value: 'vaa'
+		},
+	'िव': {
+		value: 'vi'
+		},
+	वी: {
+		value: 'vii'
+		},
+	वु: {
+		value: 'vu'
+		},
+	वू: {
+		value: 'voo'
+		},
+	वे: {
+		value: 've'
+		},
+	वै: {
+		value: 'vai'
+		},
+	वो: {
+		value: 'vo'
+	},
+	वौ: {
+		value: 'vau'
+	},
+	स: {
+		value: 'sa'
+		},
+	सा: {
+		value: 'saa'
+		},
+	'िस': {
+		value: 'si'
+		},
+	सी: {
+		value: 'sii'
+		},
+	सु: {
+		value: 'su'
+		},
+	सू: {
+		value: 'soo'
+		},
+	से: {
+		value: 'se'
+		},
+	सै: {
+		value: 'sai'
+		},
+	सो: {
+		value: 'so'
+	},
+	सौ: {
+		value: 'sau'
+	},
+	श: {
+		value: 'sha'
+		},
+	शा: {
+		value: 'sha'
+		},
+	'िश': {
+		value: 'shi'
+		},
+	शी: {
+		value: 'shii'
+		},
+	शु: {
+		value: 'shu'
+		},
+	शू: {
+		value: 'shoo'
+		},
+	शे: {
+		value: 'she'
+		},
+	शै: {
+		value: 'shai'
+		},
+	शो: {
+		value: 'sho'
+	},
+	शौ: {
+		value: 'shau'
+	},
+	ह: {
+		value: 'ha'
+		},
+	हा: {
+		value: 'haa'
+		},
+	'िह': {
+		value: 'hi'
+		},
+	ही: {
+		value: 'hii'
+		},
+	हु: {
+		value: 'hu'
+		},
+	हू: {
+		value: 'hoo'
+		},
+	हे: {
+		value: 'he'
+		},
+	है: {
+		value: 'hai'
+		},
+	हो: {
+		value: 'ho'
+	},
+	हौ: {
+		value: 'hau'
+	},
+
+//AAKAR
+	'': {
+		value: ''
+		},
+	'ा': {
+		value: 'aa'
+		},
+	'ि': {
+		value: 'i'
+		},
+	'ी': {
+		value: 'ee'
+		},
+	'ु': {
+		value: 'u'
+		},
+	'ू': {
+		value: 'oo'
+		},
+	'ॄ': {
+		value: 'Ri'
+		},
+	'े': {
+		value: 'e'
+		},
+	'ै': {
+		value: 'ai'
+		},
+	'ो': {
+		value: 'o'
+		},
+	'ौ': {
+		value: 'au'
+		},
+	'ए': {
+		value: 'ae'
+		},
+	'ः': {
+		value: 'aw'
+		},
+
+//HALF FORMS
+	क्: {
+		value: 'k'
+		},
+	ख्: {
+		value: 'kh'
+		},
+	ग्: {
+		value: 'g'
+		},
+	घ्: {
+		value: 'gh'
+		},
+	ङ्: {
+		value: 'NG'
+		},
+	च्: {
+		value: 'c'
+		},
+	छ्: {
+		value: 'ch'
+		},
+	ज्: {
+		value: 'j'
+		},
+	झ्: {
+		value: 'jh'
+		},
+	ञ्: {
+		value: 'NJ'
+		},
+	ट्: {
+		value: 'T'
+		},
+	ठ्: {
+		value: 'Th'
+		},
+	ड्: {
+		value: 'D'
+		},
+	ढ्: {
+		value: 'Dh'
+		},
+	ण्: {
+		value: 'NN'
+		},
+	त्: {
+		value: 't'
+		},
+	थ्: {
+		value: 'th'
+		},
+	द्: {
+		value: 'd'
+		},
+	ध्: {
+		value: 'dh'
+		},
+	न्: {
+		value: 'n'
+		},
+	प्: {
+		value: 'p'
+		},
+	फ्: {
+		value: 'ph'
+		},
+	फ्: {
+		value: 'f'
+		},
+	ब्: {
+		value: 'b'
+		},
+	भ्: {
+		value: 'bh'
+		},
+	म्: {
+		value: 'm'
+		},
+	य्: {
+		value: 'y'
+		},
+	र्: {
+		value: 'r'
+		},
+	ल्: {
+		value: 'l'
+		},
+	व्: {
+		value: 'v'
+		},
+	स्: {
+		value: 's'
+		},
+	श्: {
+		value: 'sh'
+		},
+	ह्: {
+		value: 'h'
+		},
+	
+
+//JOD_AKSHAR
+	क्ष: {
+		value: 'ksha'
+		},
+	त्र: {
+		value: 'tra '
+		},
+	ज्ञ: {
+		value: 'gya'
+		},
+	श्र: {
+		value: 'shra '
+		},
+
+	क्ष्: {
+		value: 'ksh'
+		},
+	त्र्: {
+		value: 'tr'
+		},
+	ज्ञ्: {
+		value: 'gy'
+		},
+	श्र्: {
+		value: 'shr'
+		},
+
+//	SPECIAL FORMS
+	रु: {
+		value : 'ru '
+	},
+
+//	NUMBERS
+	'०': {
+		value : '0'
+	},
+	'१': {
+		value : '1'
+	},
+	'२': {
+		value : '2'
+	},
+	'३': {
+		value : '3'
+	},
+	'४': {
+		value : '4'
+	},
+	'५': {
+		value : '5'
+	},
+	'६': {
+		value : '6'
+	},
+	'७': {
+		value : '7'
+	},
+	'८': {
+		value : '8'
+	},
+	'९': {
+		value : '9'
+	},
+
+//PUNCTUATIONS
+	'।': {
+		value : '.'
+	},
+	' ': {
+		value : ' '
+	},
+	'?': {
+		value : '?'
+	}
+}
+
+var objVowels = {
+		//	MATRA
+	अ: {
+		value: 'a'
+		},
+	आ: {
+		value: 'aa'
+		},
+	इ: {
+		value: 'e'
+		},
+	ई: {
+		value: 'ee'
+		},
+	उ: {
+		value: 'u'
+		},
+	ऊ: {
+		value: 'oo'
+		},
+	ॠ: {
+		value: 'Ri'
+		},
+	ए: {
+		value: 'ae'
+		},
+	ऐ: {
+		value: 'ai'
+		},
+	ओ: {
+		value: 'o'
+		},
+	औ: {
+		value: 'au'
+		},
+	ए: {
+		value: 'ae'
+		},
+	अः: {
+		value: 'aw'
+		}
+	
+
+}
+
+
+//END OF ASSIGNMENT OF KEY-VALUE PAIR for nepali - English transliteration
+
+
+//FUNCTION FOR TRANSLITERATION
+function myFunction() {
+	document.getElementById("transliteratedTextArea").innerHTML = "";
+//count number of total keys in obj
+   let count=0;
+   for(let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+         ++count;
+      }
+   }
+	
+  let data = document.getElementById("myForm").notTransliterated.value;
+  //let data = x.toLowerCase();
+	let length = data.length;
+ 	let val = '';
+	
+	//5. assign new length after addition of necessary letters
+	length = data.length;
+	
+	for( i = 0; i < length; i = i + 1){
+  	//letters at position 'i' where i = 0 to the length of data
+  	l1 = data.charAt(i);
+  	l2 = data.charAt(i+1);
+  	l3 = data.charAt(i+2);
+  	l4 = data.charAt(i+3);
+  	l0 = data.charAt(i-0);
+  	console.log(l1);
+
+		let b4 = l1 + l2 + l3 + l4;
+		let b3 = l1 + l2 + l3;
+		let b2 = l1 + l2;
+		let b1 = l1;
+
+		if(l1 == ' '){
+			if(l2 == 'a' || l2 == 'e' || l2 == 'o' || l2 == 'R' ){
+				y1 = l2;
+				y2 = l2  + l3;
+				if(y2 in objVowels){
+					val = val + ' ' + objVowels[y2].value;
+					i += 2;
+					continue;
+				}
+				else if(y1 in objVowels){
+					console.log('yes' + y1 + objVowels[y1].value);
+					val = val + ' '  + objVowels[y1].value;
+					i++;
+					continue;
+				}
+			}
+		}
+
+			if(b4 in obj){
+		val = val + obj[b4].value;
+		console.log(b4+ obj[b4].value);
+		i = i + 3;
+	}else if(b3 in obj){
+		val = val + obj[b3].value;
+		console.log(b3+ obj[b3].value);
+		i = i + 2;
+		}
+	else if(b2 in obj){
+		val = val + obj[b2].value;
+		console.log(b2 + obj[b2].value);
+		i = i + 1;
+		}
+	else if(b1 in obj){
+		val = val + obj[b1].value;
+		console.log(b1 + obj[b1].value);
+		}
+	
+
+	}	
+	console.log('VAL = ' + val);
+	document.getElementById('transliteratedTextArea').innerHTML += val;
+
+}
